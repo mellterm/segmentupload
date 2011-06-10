272eef7a1b708567c3777fac1839a09bf7c199c2
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-06-10 21:36:49 +0530

    added thin and stalker configs

---
+++ config/stalker.god.rb
@@ -0,0 +1,40 @@
+RAILS_ROOT = File.expand_path("../..", __FILE__)
+
+God.watch do |w|
+  w.name = "document-processor"
+  w.interval = 30.seconds
+  w.env = {"RAILS_ENV" => "production"}
+  w.start = "/home/ubuntu/.rvm/bin/bootup_stalk #{RAILS_ROOT}/config/jobs.rb"
+  w.log = "#{RAILS_ROOT}/log/stalker.log"
+
+  w.start_if do |start|
+    start.condition(:process_running) do |c|
+      c.running = false
+    end
+  end
+
+  w.restart_if do |restart|
+    restart.condition(:memory_usage) do |c|
+      c.above = 50.megabytes
+      c.times = [3, 5] # 3 out of 5 intervals
+    end
+
+    restart.condition(:cpu_usage) do |c|
+      c.above = 50.percent
+      c.times = 5
+    end
+  end
+
+  w.lifecycle do |on|
+    on.condition(:flapping) do |c|
+      c.to_state = [:start, :restart]
+      c.times = 5
+      c.within = 5.minute
+      c.transition = :unmonitored
+      c.retry_in = 10.minutes
+      c.retry_times = 5
+      c.retry_within = 2.hours
+    end
+  end
+end
+
